1111108b7bac78ca3b083c20f4a55c95226daab7
loveguan <zhouguanjie@qq.com>
2016-05-11 22:11:10 +0800

    version 1

--- main/index.py
+++ main/index.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 #_*_ coding:utf-8 _*_
 
-from file import demo
+def login(username):
+    if username == 'zhouguanjie':
+        return 'ok'
+    else:
+        return 'error'
+def detatil(user):
+    print '--------'
 
-demo.Foo()
+if __name__ == '__main__':
+    user = raw_input("please input you name:")
+    res = login(user)
+    if res == 'ok':
+        detatil(user)
+    else:
+        print 'wrong user'
+    
