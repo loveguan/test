db92f25db3ec0624fd1dbc5b3852b1850cab840c
loveguan <zhouguanjie@qq.com>
2016-05-17 17:22:38 +0800

    3

--- chakan.py
+++ chakan.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
 #_*_ coding:utf-8 _*_
-class Chakan:
-    
+
+#反射的应用
+
+text = raw_input("please input your path")
+arry = text.split('/')
+t = __import__('file.'+arry[0])
+model = getattr(t, arry[0])
+func=getattr(model,arry[1])
+func()
+
+
+ 
--- main/index.py
+++ main/index.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 #_*_ coding:utf-8 _*_
 
-def login(username):
-    if username == 'zhouguanjie':
-        return 'ok'
-    else:
-        return 'error'
-def detatil(user):
-    print '--------'
+# def login(username):
+#     if username == 'zhouguanjie':
+#         return 'ok'
+#     else:
+#         return 'error'
+# def detatil(user):
+#     print '--------'
+# 
+# if __name__ == '__main__':
+#     user = raw_input("please input you name:")
+#     res = login(user)
+#     if res == 'ok':
+#         detatil(user)
+#     else:
+#         print 'wrong user'
 
-if __name__ == '__main__':
-    user = raw_input("please input you name:")
-    res = login(user)
-    if res == 'ok':
-        detatil(user)
-    else:
-        print 'wrong user'
-    
+text = raw_input("please input your path")
+str1 = text.split('/')
+t = __import__('file'+'demo')
