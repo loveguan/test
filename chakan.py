629fcf3d2afd34e58f16374843c26cca2df281d8
loveguan <zhouguanjie@qq.com>
2016-05-17 17:27:03 +0800

    9

--- chakan.py
+++ chakan.py
@@ -5,8 +5,10 @@
 
 text = raw_input("please input your path")
 arry = text.split('/')
-t = __import__('file.'+arry[0])
-model = getattr(t, arry[0])
+t = __import__('file.'+arry[0])  #xiangdangyu lujing
+print t
+model = getattr(t, arry[0]) #xiang dangyu huoqu mokuai
+print model
 func=getattr(model,arry[1])
 func()
 
